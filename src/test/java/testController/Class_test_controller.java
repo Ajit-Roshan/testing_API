b0ffb76ef6342bf4b70611c29bc3d1ce7699d24a
ajit <roshanAjit@test>
2024-05-10 13:35:59 +0530

    base commit
    Change-Id: I890a48035c2a637856e7879fc90a0927ebbbacfb

---
+++ src/test/java/testController/Class_test_controller.java
@@ -0,0 +1,11 @@
+package testController;
+
+public class Class_test_controller {
+
+	public static void main(String[] args) {
+		
+		Class_test_controller controller= new Class_test_controller();
+		
+	}
+	
+}
